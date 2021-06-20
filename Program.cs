0c8c462c10353896837852a251bae44b0cede302
Todd <tnordfeldt@gmail.com>
2021-06-21 01:36:51 +0200

    Added incursion states, local loading and avg time.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ using System.Collections.Generic;
 using HtmlAgilityPack;
 using System.Threading;
 using System.Text.Json;
+using System.Globalization;
 
 namespace IncursionScraper
 {
+    enum EventType
+    {
+        Established,
+        Mobilizing,
+        Withdrawing,
+        Ended,
+        Unkown
+    }
     class Event
     {
         public string Date { get; set; }
@@ -16,14 +25,90 @@ namespace IncursionScraper
         public string Staging { get; set; }
         public string Status { get; set; }
         public string Duration { get; set; }
+        public DateTime ToDateTime()
+        {
+            string pattern = "MM.dd.yyyy HH:mm";
+            string timestr = Date.Substring(Date.IndexOf(", ")+2) + " " + Time;
+            DateTime ret = DateTime.ParseExact(timestr, pattern, CultureInfo.InvariantCulture);
+            return ret;
+        }
+        public EventType GetEventType()
+        {
+            if (Status == "Established")
+                return EventType.Established;
+            if (Status == "Mobilizing")
+                return EventType.Mobilizing;
+            if (Status == "Withdrawing")
+                return EventType.Withdrawing;
+            if (Status == "Ended")
+                return EventType.Ended;
+            return EventType.Unkown;
+        }
+    }
+    class Incursion
+    {
+        public TimeSpan TimeAlive { get; set; }
+        private Event location;
+        public DateTime Started { get; set; }
+        public DateTime Mobilized { get; set; }
+        public DateTime Withdrawing { get; set; }
+        public DateTime Ended { get; set; }
+        public Incursion(Event established)
+        {
+            Started = established.ToDateTime();
+            location = established;
+        }
+        // Return true on update if the incursion ended
+        public bool Update(Event ev)
+        {
+            switch(ev.GetEventType())
+            {
+                case EventType.Mobilizing:
+                    Mobilized = ev.ToDateTime();
+                    break;
+                case EventType.Withdrawing:
+                    Withdrawing = ev.ToDateTime();
+                    break;
+                case EventType.Ended:
+                    Ended = ev.ToDateTime();
+                    TimeAlive = Ended - Started;
+                    return true;
+                default:
+                    return false;
+            }
+            return false;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            string fn = "dump.json";
+            List<Event> events;
+            events = ReadFromFile(fn);
+            events[0].ToDateTime();
+            List<Incursion> incursions = new List<Incursion>();
+            Dictionary<string, Incursion> opened = new Dictionary<string, Incursion>();
+            events.Reverse();
+            foreach(Event ev in events)
+            {
+                if(!opened.ContainsKey(ev.Staging))
+                    opened.Add(ev.Staging, new Incursion(ev));
+                if (opened[ev.Staging].Update(ev))
+                {
+                    incursions.Add(opened[ev.Staging]);
+                    opened.Remove(ev.Staging);
+                }
+            }
+            var AvgTime = AverageTime(incursions);
+            Console.WriteLine("Number of incursions finished: {0}\nNumber of incursions still open: {1}\n", incursions.Count, opened.Count);
+            Console.WriteLine("Average time incursions have been up: {0} days, {1} hours, {2} minutes", AvgTime.Days, AvgTime.Hours, AvgTime.Minutes);
+        }
+
+        public List<Event> ScrapeSite(string fn)
+        {
             List<Event> events = new List<Event>();
             HtmlWeb req = new HtmlWeb();
-            string fn = "dump.json";
             int pagecounter = 1;
             bool run = true;
             do
@@ -62,9 +147,30 @@ namespace IncursionScraper
                 else
                     run = false;
             } while (run == true);
-
+    
             string json = JsonSerializer.Serialize(events);
             File.WriteAllText(fn, json);
+            return events;
+        }
+        static public List<Event> ReadFromFile(string fn)
+        {
+            List<Event> events;
+            using (StreamReader f = new StreamReader(fn))
+            {
+                string json = f.ReadToEnd();
+                events = JsonSerializer.Deserialize<List<Event>>(json);
+            }
+            return events;
+        }
+        static public TimeSpan AverageTime(List<Incursion> incursions)
+        {
+            //List<TimeSpan> dtl = new List<TimeSpan>();
+            TimeSpan ts = TimeSpan.Zero;
+            foreach(Incursion inc in incursions)
+            {
+                ts += inc.TimeAlive;
+            }
+            return new TimeSpan(ts.Ticks / incursions.Count);
         }
     }
 }
